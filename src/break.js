e5513c8cb5d0d4f421af0350afcc230d8516cae7
Sky-tales <deyan.tucev2007@gmail.com>
2024-09-13 14:38:04 +0200

    feat : break on time

--- src/break.js
+++ src/break.js
@@ -1,8 +1,12 @@
 const numbers = [];
 for (let i = 0; i < 10; i++) {
-    numbers.push(Math.floor(Math.random() * 100));
+    numbers.push(Math.floor(Math.random() * 100))
 }
 
 for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
+    console.log(numbers[i])
+    if (numbers[i] >= 50) {
+        console.log("il a économisé "+(9-i)+" essais")
+        break
+    }
 }
